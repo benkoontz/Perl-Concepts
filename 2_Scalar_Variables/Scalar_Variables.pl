2cf729b679e72fe890066d41d5e1d8b01db3cd37
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 15:02:13 -0700

    Create Scalar_Variables.pl

---
+++ 2_Scalar_Variables/Scalar_Variables.pl
@@ -0,0 +1,16 @@
+
+# a scalar variable is a single unit of data
+# scalar variables can hold numeric, string, octal, etc...
+
+$rank=1;
+$name="Ben";
+$total_money=55555555.55;
+$my_oct=0167; # for octals you place a 0 at the beginning
+$my_hex=0xabe; # use 0x to create a hexadecimal number
+
+print "Name: $name\n";
+print "Rank: $rank\n";
+print "Total Prize Money: $total_money\n";
+
+print"My Octal Number: $my_oct\n";
+print "My Hexadecimal Number: $my_hex\n";
