fc711444e0edf653529a699e43aa12bb527b0cce
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 15:26:06 -0700

    Create perl.pl

---
+++ 5_Array-Operations/perl.pl
@@ -0,0 +1,11 @@
+# array operations
+
+@ranks = (1..10); # two dots if for filling sequential data, puts the numbers between 1 and 10
+@alphabets= (a..z); # puts the letters between a and z
+
+print "All ranks: @ranks\n";
+print "All Alphabets: @alphabets\n";
+
+$size=@alphabets; # prints 26 for the number of elements
+
+print "Size of alphabets array: $size\n";
