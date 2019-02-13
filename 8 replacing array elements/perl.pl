6c316bd17de78e189f6e0e283e48633359df03cd
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 15:52:31 -0700

    Create perl.pl

---
+++ 8 replacing array elements/perl.pl
@@ -0,0 +1,12 @@
+# Slicing Arrays is getting specific parts of an array
+# the splice function is used to replace one ore more elements in an array
+
+@actors=('Tom hanks', 'Brad pitt', 'Will Smith', 'Edward Norton');
+@new=('Leonardo Dicaproio', 'denzel washington');
+
+print "@actors\n"; # displays elements in actors variable
+
+# first parameter is the name of array to remove elements, second is the the offset position, third argument is number of elements to replace, last argument is array to replace elements with 
+splice(@actors, 1, 2, @new); # replaces brad pitt and will smith with leonardo dica
+
+print "@actors\n";
