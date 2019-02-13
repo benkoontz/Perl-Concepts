43243b51dca6dae7ec1066a66b905d15c73d4a13
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-13 14:52:35 -0700

    Create quotes.pl

---
+++ Comments and Quotes/quotes.pl
@@ -0,0 +1,12 @@
+
+
+#This is a comment and it will be 
+# ignored by perl
+
+print "hello world\n";
+print 'hello world\n'; # \n won't work, this won't go to newline
+
+$my_var = 20;
+
+print "\nvalue of my variable: $my_var\n";
+print 'value of my variable: $my_var\n'; # single quotes wont display values of variables
