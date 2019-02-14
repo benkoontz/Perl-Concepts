f3082ef57b6851268ec26450a3f7db01a2479a7a
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 14:07:21 -0700

    Create perl.pl

---
+++ 48 renaming files/perl.pl
@@ -0,0 +1,10 @@
+# Renaming files
+
+if (rename("C:/Users/Ron/Documents/Perl/Folder/Ben.txt","C:/Users/Ron/Documents/Perl/Folder/Ben2.txt"))
+{
+	print "file renamed successfully!\n";
+}
+else
+{
+	print "unable to rename file \n";
+}
