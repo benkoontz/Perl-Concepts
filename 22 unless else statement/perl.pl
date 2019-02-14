c761a356528e21214b578d945410a6f63c52b187
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 11:09:48 -0700

    Create perl.pl

---
+++ 22 unless else statement/perl.pl
@@ -0,0 +1,12 @@
+# unless else statement  if the opposite of if and else
+
+$x=10;
+
+unless($x%2==0) # check values of x that are not even
+{
+	print "x is odd\n";
+}
+else
+{
+	print "x is even\n";
+}
