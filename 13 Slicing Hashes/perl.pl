93f8e63e169f4062790fb04ca2b8ee02dc6bd46c
benkoontz <36342241+benkoontz@users.noreply.github.com>
2019-02-14 10:15:40 -0700

    Create  perl.pl

---
+++ 13 Slicing Hashes/perl.pl
@@ -0,0 +1,14 @@
+# slicing hashes
+# a hash is a set of key value pairs
+# a phonebook could be an example of a hash, name is a key, phone number is a key
+
+# %symbol is used to make a hash
+# you put key first and value second
+
+
+%players=(1=>"Novak", 2=>"Roger", 3=>"Andy", 4=>"Kei"); # hash for players
+
+# use arrays to slice the hash
+@my_list=@players{2..4}; # gets roger, andy, and kei
+
+print "Top tenis players in the world: @my_list\n";
